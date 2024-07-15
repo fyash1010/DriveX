41e5ae9a120a6eef463d1c8248af2b6547e05e5d
fyash1010 <146874433+fyash1010@users.noreply.github.com>
2024-07-15 14:46:08 +0530

    .

--- Face Detection/faceLandmark.py
+++ Face Detection/faceLandmark.py
@@ -1,16 +1,20 @@
+# Import required libraries
 import cv2
 import dlib
 import keyboard
 
+# Initialize openCV video capture
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+# Initialize DLIB
 detector = dlib.get_frontal_face_detector()
-dlib_facelandmark = dlib.shape_predictor("C:/Users/Fnu Yash/Desktop/Python/Face_Detection/shape_predictor_68_face_landmarks.dat")
+dlib_facelandmark = dlib.shape_predictor("C:\Users\yashr\Home\Python\Face_Detection\shape_predictor_68_face_landmarks.dat") # FIX PATH AS NEEDED
 
 while True:
     _, frame = cap.read()
     grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    # Plot landmarks
     faces = detector(grayImage)
     for face in faces:
         face_landmarks = dlib_facelandmark(grayImage, face)
@@ -23,8 +27,10 @@ while True:
     cv2.imshow("Face Landmarks", frame)
     cv2.waitKey(1)
 
+    # Exit on 'q'
     if keyboard.is_pressed('q'):
         break
 
+# Release cv window
 cap.release()
 cv2.destroyAllWindows
